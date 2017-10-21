e348dc8f88cfa2c30f2f7502173598a138a7b973
silvareal <sylvernusakubo@yahoo.com>
2017-10-21 16:31:45 +0100

    updated

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third party
+    'pagedown',
+
+    #local app
     'posts.apps.PostConfig',
 ]
 
