b789d5e2481be84bb6f97ef96099a16642ba4933
silvareal <sylvernusakubo@yahoo.com>
2017-11-03 03:33:50 +0100

    TEM DIR

--- blog/settings.py
+++ blog/settings.py
@@ -59,12 +59,11 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'blog.urls'
 
-PROJECT_DIR =os.path.dirname(__file__)
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
